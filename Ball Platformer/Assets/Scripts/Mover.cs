8585ffa907e440957d7cea0974a9231ef1ba6aa2
Anthony L <acling2k@gmail.com>
2016-05-04 00:23:20 -0700

    Starting Commit

---
+++ Ball Platformer/Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float x;
+    public float y;
+    public float z;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        rb.velocity = new Vector3(x, y, z);
+	}
+}
