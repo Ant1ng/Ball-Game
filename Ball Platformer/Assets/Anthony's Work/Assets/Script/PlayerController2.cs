8a114cfa1d7d6a70fac8951a0fddedde34c9d29c
Anthony L <acling2k@gmail.com>
2016-05-06 15:27:04 -0700

    Placed Erik's work.

---
+++ Ball Platformer/Assets/Anthony's Work/Assets/Script/PlayerController2.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController2 : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float speed;
+    public float forceMag;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //bool force = false;
+
+        Vector3 v = rb.velocity;
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        float z = 0.0f;
+
+        //if (v.y >= 0.5f || v.y <= -0.5f)
+        //{
+        //    force = true;
+        //}
+
+        if (Input.GetKey(KeyCode.M) && rb.velocity.y == 0.0f)
+        {
+            z = 5.0f;
+        }
+
+        v.x = speed * x;
+        v.y = v.y + z;
+        v.z = speed * y;
+
+        //if (!force)
+        //{
+            rb.velocity = v;
+        //}
+
+        //if (force)
+        //{
+        //    rb.AddForce(forceMag * new Vector3(x, y, z));
+        //}
+    }
+}
---
+++ Ball Platformer/Assets/Erik's Work/CameraController2.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController: MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 offset;
+
+    private Vector3 ballPosition;
+    private Quaternion angle;
+
+    private Rigidbody rb;
+    private Rigidbody rb1;
+        
+    void Postion(GameObject ball)
+    {
+        player = ball;
+    }
+    
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb1 = player.GetComponent<Rigidbody>();
+        rb.position = rb1.position + offset;
+        angle = rb1.rotation;
+    }
+
+    void Update ()
+    {
+        rb.position = rb1.position + offset;
+	}
+}
