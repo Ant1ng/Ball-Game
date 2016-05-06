fe28f2e0d529642994e0d20aa3922fe9cacd6e62
Erik <erik101.3@comcast.net>
2016-05-06 15:48:15 -0700

    Added Javascript file, and changed some C# file names.

---
+++ Ball Platformer/Assets/Erik's Work/CameraControl.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var player : GameObject;
+
+var camera : GameObject;
+
+function Start () {
+
+//Use Unity to postion the camera the distance you want it to stay from the ball. This script will keep it that distance.
+
+}
+
+function Update () {
+
+	if(Input.GetAxisRaw("Horizontal" < 0)){
+
+	}
+
+}
