1ed94fbe8985b9253a682f15d472fff77f5a31fb
Anthony L <acling2k@gmail.com>
2016-05-12 13:05:48 -0700

    Editted GameController

--- Ball Platformer/Assets/Leon's Work/GameController.cs
+++ Ball Platformer/Assets/Leon's Work/GameController.cs
@@ -1,18 +1,46 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameController : MonoBehaviour {
 	private int score;
 	private int lives;
+
+    public Text scoreText;
+    public Text livesText;
 	
-	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        score = 0;
+        lives = 3;
+        SetScoreText();
+        SetLivesText();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    
 	}
-	
+
+    void addScore (int add)
+    {
+        score = score + add;
+        SetScoreText();
+    }
+
+    void gainLives (int add)
+    {
+        lives = lives + add;
+        SetLivesText();
+    }
+    
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score;
+    }
+    
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives;
+    }	
 }
