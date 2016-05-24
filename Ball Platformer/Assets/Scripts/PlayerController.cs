4de5c9bff21b83a5a6dfa5c64d8cc5882d060517
Anthony L <acling2k@gmail.com>
2016-05-24 12:11:15 -0700

    Organized the files and deleted some unneccessary files.

---
+++ Ball Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public float speed;
+    public float jump;
+
+    public GameController gameController;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+        //bool force = false;
+
+        //Vector3 v = rb.velocity;
+        //float x = Input.GetAxis("Horizontal");
+        //float y = Input.GetAxis("Vertical");
+        //float z = 0.0f;
+
+        //gameController.scoreText.text = "Velocity: " + rb.velocity.y;
+
+        //if (Input.GetKey(KeyCode.M) && rb.velocity.y == 0.0f)
+        //{
+        //    z = jump;
+        //}
+
+        //v.x = speed * x;
+        //v.y = v.y + z;
+        //v.z = speed * y;
+
+        //rb.velocity = v;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("shit"))
+        {
+            other.gameObject.SetActive(false);
+            gameController.addScore(1);
+        }
+    }
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        float z = 0.0f;
+
+        if (Input.GetKey(KeyCode.M))
+        {
+            z = jump;
+        }
+
+        transform.Translate(new Vector3(x, z/speed, y) * speed * Time.smoothDeltaTime);
+    }
+}
---
+++ Ball Platformer/Assets/Scripts/Spring.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour {
+
+    public GameObject ball;
+    
+    // Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (gameObject.GetComponent<sprong>().enabled)
+            other.GetComponent<Rigidbody>().velocity = new Vector3 (0.0f, 50.0f, 0.0f);
+    }
+}
